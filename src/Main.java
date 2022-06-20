3f484db639b6dcb31607f112d5b276d7979f3e24
avvasil <avvasil@mail.ru>
2022-06-20 18:27:14 +0300

    Test 11

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        mapToString();
+    }
+
+    /*Дан массив пар названий книг и авторов,
+    необходимо привести каждую пару в строку вида:
+    “Название книги” Имя Отчество Фамилия автора
+     */
+
+    public static void mapToString() {
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Как стать знаменитым.", "Полишинель.");
+        map.put("Закат в Вегасе", "Дж. Дж. Коуч.");
+
+        for (Map.Entry<String, String> entry : map.entrySet())
+        {
+            System.out.println('"' + entry.getKey() + '"' + ", " + entry.getValue());
+        }
     }
 }
